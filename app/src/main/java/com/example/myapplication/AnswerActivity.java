671d85ab942886de5194c297eecf91ed1c5f22db
ivan-bulkin <oleg@6516363.ru>
2021-08-21 14:00:30 +0300

    Лабораторная работа №12 (Android) завершение разработки первого приложения

--- app/src/main/java/com/example/myapplication/AnswerActivity.java
+++ app/src/main/java/com/example/myapplication/AnswerActivity.java
@@ -1,27 +1,72 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AnswerActivity extends AppCompatActivity {
     private TextView answerTextView;
-    private boolean isAnswerTrue;
+    private TextView answersUserTextView;
+    private int correctAnswer;
+    private Button startAgainBtn;
+    private boolean[] answersUser;//создаём массив, в котором будем хранить ответы пользователя
+//Так не получилось(будем ещё передавать количество элементов массива и создадим его в onCreate):
+//    boolean[] answersUser = new boolean[5];//создаём массив, в котором будем хранить ответы пользователя
+//    private ArrayList<Boolean> answersUser = new ArrayList();//создаём массив, в котором будем хранить ответы пользователя создаём здесь ArrayList чтобы не мучиться с передачей размерности массива
+
+    //    private boolean isAnswerTrue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+        startAgainBtn = findViewById(R.id.startAgainBtn);
 
-        isAnswerTrue = getIntent().getBooleanExtra("answer", false);
+        correctAnswer = getIntent().getIntExtra("correctAnswer", 0);//получаем количество правильных ответов от другой активности
+        answersUser = getIntent().getBooleanArrayExtra("answersUser");//Получаем элементы массива с ответами пользователя
+        answerTextView = findViewById(R.id.answerTextView);//находим элемент answerTextView на активности activity_answer
+        answerTextView.setText(Integer.toString(correctAnswer));//выводим количество правильных ответов
+        answersUserTextView = findViewById(R.id.answersUserTextView);//находим элемент answersUserTextView на активности activity_answer
+        answersUserTextView.setText("Вы ответили на вопросы:" + "\n");
+        for (int i = 0; i < answersUser.length; i++) {
+            answersUserTextView.append((i + 1) + " " + (answersUser[i] ? "Правильно" : "Не правильно") + "\n");//используем модный тернарный оператор
+        }
+        //нажатие кнопки Начать заново,
+        startAgainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnswerActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
 
-        answerTextView = findViewById(R.id.answerTextView);
+//        isAnswerTrue = getIntent().getBooleanExtra("answer", false);
 /*        if (isAnswerTrue)
             answerTextView.setText(R.string.yes);
         else
             answerTextView.setText(R.string.no);*/
         //тоже самое, что выше только с помощью тернарного оператора и одной строчкой !!!
         //в тернарном операторе мы можем выполнить только одну инструкцию!!!
-        answerTextView.setText(isAnswerTrue ? R.string.yes : R.string.no);
-    }
-}
+//        answerTextView.setText(isAnswerTrue ? R.string.yes : R.string.no);
+
+
+/*
+    Лабораторная работа №12 (Android) завершение разработки первого приложения
+
+        Задание
+
+        * 1) Записываем строку "Вопрос - ваш ответ да/нет" в массив
+        * 2) Складываем правильные ответы в переменню int
+        * 3) Проверяем, что вопрос последний
+        * 4) Если вопрос последний, то создаём интент
+        * 5) Добавляем дополнение в Интент (массив с вопросамии ответами)
+        * 6) Добавляем дополнение в Интент (int с числом правильных ответов)
+        * 7) Запускаем активность
+        * 8) На запущенной активности вывести строки из массива, тем самым отобразив вопросы и ответы пользователя*/
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button yesBtn;
     Button noBtn;
-    Button showAnswer;
+    //    Button showAnswer;
     TextView questionTextView;
     Question[] questions = {
             new Question(R.string.question1, true),
@@ -23,28 +23,39 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question5, false)
     };
     int questionIndex = 0;
+    int correctAnswer = 0;//переменная, в которой будет храниться количество правильных ответов
+    boolean[] answersUser = new boolean[questions.length];//создаём массив, в котором будем хранить ответы пользователя
 
-    @Override//Событие, которое срабатывает при повороте экрана
+    @Override
+//Событие, которое срабатывает при повороте экрана. Сохраняем тут значение переменных, чтобы их потом восстановить
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.d("SYSTEM INFO:", "Вызван метод onSaveInstanceState");
-        savedInstanceState.putInt("CurrentQuestionIndex", questionIndex);//Сохраняем текущий номер вопроса
+        savedInstanceState.putInt("currentQuestionIndex", questionIndex);//Сохраняем текущий номер вопроса
+        savedInstanceState.putInt("correctAnswer", correctAnswer);//Сохраняем количество правильных ответов
+        savedInstanceState.putBooleanArray("answersUser", answersUser);//Сохраняем массив с ответами пользователя
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("SYSTEM INFO:", "Вызван метод onCreate");
+        if (questionIndex == 0)
+            correctAnswer = 0;//сбрасываем количество правильных ответов, если у нас первый вопрос. Это сделано, чтобы запускать приложение сколько угодно раз
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState != null)
-            questionIndex = savedInstanceState.getInt("CurrentQuestionIndex", 0);//Восстанавливаем номер вопроса
+        if (savedInstanceState != null) {//восстанавливаем значения при возвращении после поворота экрана
+            questionIndex = savedInstanceState.getInt("currentQuestionIndex", 0);//Восстанавливаем номер вопроса
+            correctAnswer = savedInstanceState.getInt("correctAnswer", 0);//Восстанавливаем количество правильных ответов
+            answersUser = savedInstanceState.getBooleanArray("answersUser");//Восстанавливаем массив с ответами пользователя
+        }
         yesBtn = findViewById(R.id.yesBtn);
         noBtn = findViewById(R.id.noBtn);
-        showAnswer = findViewById(R.id.showAnswer);
+//        showAnswer = findViewById(R.id.showAnswer);
         questionTextView = findViewById(R.id.questionTextView);
 
         questionTextView.setText(questions[questionIndex].getQuestionText());
 
+/*        //нажатие кнопки Показать ответы
         showAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,8 +63,9 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("answer", questions[questionIndex].isAnswerTrue());
                 startActivity(intent);
             }
-        });
+        });*/
 
+        //нажатие кнопки ДА
         yesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //нажатие кнопки НЕТ
         noBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,18 +86,29 @@ public class MainActivity extends AppCompatActivity {
     public void checkAnswer(boolean btn) {
         if ((questions[questionIndex].isAnswerTrue() && btn) || (!questions[questionIndex].isAnswerTrue() && !btn)) {
             Toast.makeText(MainActivity.this, "Правильно!", Toast.LENGTH_SHORT).show();
+            correctAnswer++;//если ответ правильный увеличиваем correctAnswer
+            answersUser[questionIndex] = true;//сохраняем ответ пользователя в массиве
         } else {
             Toast.makeText(MainActivity.this, "Не правильно!", Toast.LENGTH_SHORT).show();
-
+            answersUser[questionIndex] = false;//сохраняем ответ пользователя в массиве
         }
-        questionIndex = (questionIndex + 1) % questions.length;
-        questionTextView.setText(questions[questionIndex].getQuestionText());
+        if (questionIndex == questions.length - 1) {//при достижении последнего вопроса вызываем активность AnswerActivity
+            Intent intent = new Intent(MainActivity.this, AnswerActivity.class);
+            intent.putExtra("correctAnswer", correctAnswer);//сохраняем значение correctAnswer для передачи между активностями
+            intent.putExtra("answersUser", answersUser);//сохраняем значения массива answersUser(ответы пользователя) для передачи между активностями
+            startActivity(intent);
+        }
+        questionIndex = (questionIndex + 1) % questions.length;//хитрым образом увеличиваем questionIndex на единицу, при этом при достижении последнего вопроса(questions.length) questionIndex сбрасывается в НОЛЬ
+        questionTextView.setText(questions[questionIndex].getQuestionText());//выводим на экран следующий вопрос
     }
 
     @Override
     public void onStart() {
         super.onStart();
         Log.d("SYSTEM INFO:", "Вызван метод onStart");
+//        correctAnswer = 0;//сбрасываем количество правильных ответов на ноль, чтобы отвечать на вопросы ещё раз - это здесь не сработает, т.к. onStart срабатывает каждый раз при повороте экрана
+        if (questionIndex == 0)
+            correctAnswer = 0;//сбрасываем количество правильных ответов, если у нас первый вопрос. Это сделано, чтобы запускать приложение сколько угодно раз
     }
 
     @Override
